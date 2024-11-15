d09aeb06b1fad9e94a409f69bb09c0fe16fb21eb
Thiago de Lima de Assis Cordeiro <thiagocordeiro2512@gmail.com>
2024-11-15 00:41:09 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,20 @@
+var h = document.getElementById('hora');
+var m = document.getElementById('minuto');
+var d = document.getElementById('dia');
+
+function hora(){
+    const novaData = new Date();
+    const dataDefinida = new Date('2024-09-07T14:30:00');
+
+    const diferença = novaData.getTime() - dataDefinida.getTime();
+ 
+    const dias = Math.floor(diferença / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferença % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferença % (1000 * 60 * 60)) / (1000 * 60));
+    
+    d.textContent = dias;
+    h.textContent = horas;
+    m.textContent = minutos;
+}
+
+setInterval(hora, 1000);
